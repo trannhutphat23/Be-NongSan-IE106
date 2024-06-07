5f0dd6352ec5cfffc21f1c3b81966be6e0129628
trannhutphat23 <trannhutphattv@gmail.com>
2024-06-08 00:40:34 +0700

    fix

--- src/app.js
+++ src/app.js
@@ -9,4 +9,19 @@ app.use(cors())
 require('./database/init.mongodb')
 app.use('/', require('./router/admin.router'))
 
+app.use((req, res, next) => {
+    const err = new Error('Not Found')
+    err.status = 404
+    next(err)
+})
+
+app.use((err, req, res, next) => {
+    const statusCode = err.status || 500
+    return res.status(statusCode).json({
+        status: 'error',
+        code: statusCode,
+        message: err.message || "Internal Server Error",
+    })
+})
+
 module.exports = app;
